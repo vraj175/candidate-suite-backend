742569b3ff0aca73a44878ed30b1b22da3bf8efd
Michael Seeley <mseeley@kingsleygate.com>
2021-07-28 12:38:34 +0000

    AfterInstallHook.sh edited online with Bitbucket

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf /home/ec2-user/build/ROOT.war
-mv /home/ec2-user/build/target/kgpproductone.war /home/ec2-user/build/ROOT.war
-sudo cp /home/ec2-user/build/ROOT.war /opt/projectone/tomcat/webapps/
+mv /home/ec2-user/build/target/candidate-suite-backend-0.0.1-SNAPSHOT.war /home/ec2-user/build/ROOT.war
+sudo cp /home/ec2-user/build/ROOT.war /opt/candidatesuite/tomcat/webapps/
