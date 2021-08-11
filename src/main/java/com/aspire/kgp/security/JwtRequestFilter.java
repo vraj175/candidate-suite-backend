6f11f67a4d7687af8b8afa7fa9ad9899e15d927b
Vivek Tanna <vivek.tanna@aspiresoftware.in>
2021-08-11 16:46:46 +0530

    Feature #37515: Create API for User Login and implement Oauth2 Implementation

--- src/main/java/com/aspire/kgp/security/JwtRequestFilter.java
+++ src/main/java/com/aspire/kgp/security/JwtRequestFilter.java
@@ -65,12 +65,8 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     } else if (currentUrl.indexOf("/api/") < 0) {
       log.info("not need to authorize ");
       apiKeyValidate = false;
-    } else {
-      if (currentUrl.indexOf("/initialize") > 0 || currentUrl.indexOf("/user/invite") > 0) {
-        log.info("no need to verify JWT token ");
-      } else {
-        jwtTokenValidate = true;
-      }
+    } else if (!(currentUrl.indexOf("/initialize") > 0 || currentUrl.indexOf("/user/invite") > 0)) {
+      jwtTokenValidate = true;
     }
     
     ObjectMapper objectMapper = new ObjectMapper();
