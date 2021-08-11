5c9f97579c81b58b5a8198810186e95f361824d4
Paresh Sadvani <paresh.sadvani@aspiresoftserv.com>
2021-08-11 19:22:25 +0530

    resolve conflict

--- src/main/java/com/aspire/kgp/dto/UserDTO.java
+++ src/main/java/com/aspire/kgp/dto/UserDTO.java
@@ -16,16 +16,6 @@ public class UserDTO {
   private String email;
   @JsonIgnore
   private String token;
-  @SerializedName("email")
-  private String email;
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
 
   public String getId() {
     return id;
