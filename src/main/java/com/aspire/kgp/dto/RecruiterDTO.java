f6c5518128f9a24d8a8e4ef848451036892c8709
vraj.patel <vraj.patel@aspiresoftserv.com>
2021-08-13 17:32:29 +0530

    Feature #37602 Candidatesuite : Create an API For fetch Welcome Page details

---
+++ src/main/java/com/aspire/kgp/dto/RecruiterDTO.java
@@ -0,0 +1,15 @@
+package com.aspire.kgp.dto;
+
+public class RecruiterDTO {
+
+  private UserDTO user;
+
+  public UserDTO getUser() {
+    return user;
+  }
+
+  public void setUser(UserDTO user) {
+    this.user = user;
+  }
+
+}
