f38c057a4e92f02b90e0b9ce2842f6642675bf42
Urmil Patel <urmil.patel@aspiresoftserv.com>
2021-08-19 19:27:34 +0530

    Apply formatter

--- src/main/java/com/aspire/kgp/dto/CandidateDTO.java
+++ src/main/java/com/aspire/kgp/dto/CandidateDTO.java
@@ -7,73 +7,73 @@ import com.google.gson.annotations.SerializedName;
 
 @JsonFilter("candidateFilter")
 public class CandidateDTO {
-	private String id;
-	private UserDTO contact;
-	private SearchDTO search;
-	@SerializedName("kgp_interview_date1")
-	private String kgpInterviewDate1;
+  private String id;
+  private UserDTO contact;
+  private SearchDTO search;
+  @SerializedName("kgp_interview_date1")
+  private String kgpInterviewDate1;
 
-	@SerializedName("kgp_interview_date2")
-	private String kgpInterviewDate2;
+  @SerializedName("kgp_interview_date2")
+  private String kgpInterviewDate2;
 
-	@SerializedName("kgp_interview_date3")
-	private String kgpInterviewDate3;
-	private List<InterviewDTO> interviews;
+  @SerializedName("kgp_interview_date3")
+  private String kgpInterviewDate3;
+  private List<InterviewDTO> interviews;
 
-	public String getId() {
-		return id;
-	}
+  public String getId() {
+    return id;
+  }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+  public void setId(String id) {
+    this.id = id;
+  }
 
-	public UserDTO getContact() {
-		return contact;
-	}
+  public UserDTO getContact() {
+    return contact;
+  }
 
-	public void setContact(UserDTO contact) {
-		this.contact = contact;
-	}
+  public void setContact(UserDTO contact) {
+    this.contact = contact;
+  }
 
-	public SearchDTO getSearch() {
-		return search;
-	}
+  public SearchDTO getSearch() {
+    return search;
+  }
 
-	public void setSearch(SearchDTO search) {
-		this.search = search;
-	}
+  public void setSearch(SearchDTO search) {
+    this.search = search;
+  }
 
-	public String getKgpInterviewDate1() {
-		return kgpInterviewDate1;
-	}
+  public String getKgpInterviewDate1() {
+    return kgpInterviewDate1;
+  }
 
-	public void setKgpInterviewDate1(String kgpInterviewDate1) {
-		this.kgpInterviewDate1 = kgpInterviewDate1;
-	}
+  public void setKgpInterviewDate1(String kgpInterviewDate1) {
+    this.kgpInterviewDate1 = kgpInterviewDate1;
+  }
 
-	public String getKgpInterviewDate2() {
-		return kgpInterviewDate2;
-	}
+  public String getKgpInterviewDate2() {
+    return kgpInterviewDate2;
+  }
 
-	public void setKgpInterviewDate2(String kgpInterviewDate2) {
-		this.kgpInterviewDate2 = kgpInterviewDate2;
-	}
+  public void setKgpInterviewDate2(String kgpInterviewDate2) {
+    this.kgpInterviewDate2 = kgpInterviewDate2;
+  }
 
-	public String getKgpInterviewDate3() {
-		return kgpInterviewDate3;
-	}
+  public String getKgpInterviewDate3() {
+    return kgpInterviewDate3;
+  }
 
-	public void setKgpInterviewDate3(String kgpInterviewDate3) {
-		this.kgpInterviewDate3 = kgpInterviewDate3;
-	}
+  public void setKgpInterviewDate3(String kgpInterviewDate3) {
+    this.kgpInterviewDate3 = kgpInterviewDate3;
+  }
 
-	public List<InterviewDTO> getInterviews() {
-		return interviews;
-	}
+  public List<InterviewDTO> getInterviews() {
+    return interviews;
+  }
 
-	public void setInterviews(List<InterviewDTO> interviews) {
-		this.interviews = interviews;
-	}
+  public void setInterviews(List<InterviewDTO> interviews) {
+    this.interviews = interviews;
+  }
 
 }
