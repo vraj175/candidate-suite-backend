2e739da0e64a0417216c2d55045a9c663f801122
vraj.patel <vraj.patel@aspiresoftserv.com>
2021-09-02 15:32:16 +0530

    Feature #37615: Candidate suite : Swagger Configuration

--- src/main/java/com/aspire/kgp/CandidateSuiteBackendApplication.java
+++ src/main/java/com/aspire/kgp/CandidateSuiteBackendApplication.java
@@ -3,6 +3,8 @@ package com.aspire.kgp;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,14 +17,14 @@ import com.google.common.base.Predicates;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.SecurityReference;
-import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -39,7 +41,8 @@ public class CandidateSuiteBackendApplication {
   public OpenAPI customOpenAPI() {
     return new OpenAPI().components(new Components())
         .info(new Info().title("Candidatesuite Application API").description(
-            "This is a sample Spring Boot RESTful service using springdoc-openapi and OpenAPI 3."));
+            "This is a sample Spring Boot RESTful service using springdoc-openapi and OpenAPI 3.")
+            .version("1.0"));
   }
 
   /***
@@ -47,32 +50,73 @@ public class CandidateSuiteBackendApplication {
    * 
    * @return
    */
+  /*
+   * @Bean public Docket swaggerConfiguration() { return new
+   * Docket(DocumentationType.SWAGGER_2).select()
+   * .paths(Predicates.not(PathSelectors.ant(Constant.USER_AUTHENTICATE_API_URL)))
+   * .paths(PathSelectors.ant("/api/**")) .paths(Predicates
+   * .not(PathSelectors.ant(Constant.BASE_API_URL + Constant.PUBLIC_API_URL + "/**")))
+   * .build().securitySchemes(Arrays.asList(accessToken(), apiKey()))
+   * .securityContexts(Arrays.asList(securityContext())); }
+   * 
+   * @Bean public Docket userAuthentication() { return new
+   * Docket(DocumentationType.SWAGGER_2).groupName(Constant.USER_AUTHENTICATE_GROUP_NAME)
+   * .select().apis(httpRequestHandler())
+   * .paths(PathSelectors.ant(Constant.USER_AUTHENTICATE_API_URL)).build()
+   * .securitySchemes(Arrays.asList(apiKey()))
+   * .securityContexts(Arrays.asList(userAuthenticateSecurityContext())); }
+   * 
+   * @Bean public Docket publicAuthentication() { return new
+   * Docket(DocumentationType.SWAGGER_2).groupName(Constant.PUBLIC_GROUP_NAME).select()
+   * .paths(PathSelectors.ant(Constant.BASE_API_URL + Constant.PUBLIC_API_URL + "/**")).build()
+   * .securitySchemes(Arrays.asList(apiKey()))
+   * .securityContexts(Arrays.asList(publicSecurityContext())); }
+   */
+
   @Bean
-  public Docket swaggerConfiguration() {
-    return new Docket(DocumentationType.SWAGGER_2).select()
-        .paths(Predicates.not(PathSelectors.ant(Constant.USER_AUTHENTICATE_API_URL)))
-        .paths(PathSelectors.ant("/api/**"))
-        .paths(Predicates
-            .not(PathSelectors.ant(Constant.BASE_API_URL + Constant.PUBLIC_API_URL + "/**")))
-        .build().securitySchemes(Arrays.asList(accessToken(), apiKey()))
-        .securityContexts(Arrays.asList(securityContext()));
+  public GroupedOpenApi swaggerConfiguration() {
+    return GroupedOpenApi.builder().group("default")
+        .pathsToExclude(Constant.BASE_API_URL + Constant.PUBLIC_API_URL + "/**",
+            Constant.USER_AUTHENTICATE_API_URL)
+        .pathsToMatch("/api/v1.0/candidates/**", "/api/v1.0/searches/**", "/api/v1.0/companies/**",
+            "/api/v1.0/company/**", "/api/v1.0/companyInfo/**", "/api/v1.0/languages/**",
+            "/api/v1.0/roles/**", "/api/v1.0/profile/**", "/api/v1.0/user/**")
+        .addOpenApiCustomiser(defaultAPIConfig()).build();
+  }
+
+
+  public OpenApiCustomiser defaultAPIConfig() {
+    return openApi -> openApi
+        .components(new Components()
+            .addSecuritySchemes("Api_key",
+                new SecurityScheme().type(SecurityScheme.Type.APIKEY).in(SecurityScheme.In.HEADER)
+                    .name("Api_key"))
+            .addSecuritySchemes("Authorization",
+                new SecurityScheme().type(SecurityScheme.Type.APIKEY).in(SecurityScheme.In.HEADER)
+                    .name("Authorization")))
+        .addSecurityItem(new SecurityRequirement().addList("Api_key").addList("Authorization"));
   }
 
   @Bean
-  public Docket userAuthentication() {
-    return new Docket(DocumentationType.SWAGGER_2).groupName(Constant.USER_AUTHENTICATE_GROUP_NAME)
-        .select().apis(httpRequestHandler())
-        .paths(PathSelectors.ant(Constant.USER_AUTHENTICATE_API_URL)).build()
-        .securitySchemes(Arrays.asList(apiKey()))
-        .securityContexts(Arrays.asList(userAuthenticateSecurityContext()));
+  public GroupedOpenApi publicAuthentication() {
+    return GroupedOpenApi.builder().group(Constant.PUBLIC_GROUP_NAME)
+        .pathsToMatch(Constant.BASE_API_URL + Constant.PUBLIC_API_URL + "/**")
+        .addOpenApiCustomiser(apiConfig()).build();
+  }
+
+  public OpenApiCustomiser apiConfig() {
+    return openApi -> openApi
+        .components(
+            new Components().addSecuritySchemes("Api_key",
+                new SecurityScheme().type(SecurityScheme.Type.APIKEY).in(SecurityScheme.In.HEADER)
+                    .name("Api_key")))
+        .addSecurityItem(new SecurityRequirement().addList("Api_key"));
   }
 
   @Bean
-  public Docket publicAuthentication() {
-    return new Docket(DocumentationType.SWAGGER_2).groupName(Constant.PUBLIC_GROUP_NAME).select()
-        .paths(PathSelectors.ant(Constant.BASE_API_URL + Constant.PUBLIC_API_URL + "/**")).build()
-        .securitySchemes(Arrays.asList(apiKey()))
-        .securityContexts(Arrays.asList(publicSecurityContext()));
+  public GroupedOpenApi userAuthentication() {
+    return GroupedOpenApi.builder().group(Constant.USER_AUTHENTICATE_GROUP_NAME)
+        .pathsToMatch(Constant.USER_AUTHENTICATE_API_URL).addOpenApiCustomiser(apiConfig()).build();
   }
 
   private ApiKey apiKey() {
