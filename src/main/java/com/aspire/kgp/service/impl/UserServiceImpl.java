0bc7239ab161d51c161f28f53df98be122babe28
Vivek Tanna <vivek.tanna@aspiresoftware.in>
2021-09-03 19:27:38 +0530

    Feature #37692: Candidate Suite : Write application test case.

--- src/main/java/com/aspire/kgp/service/impl/UserServiceImpl.java
+++ src/main/java/com/aspire/kgp/service/impl/UserServiceImpl.java
@@ -228,7 +228,7 @@ public class UserServiceImpl implements UserService {
        */
       private static final long serialVersionUID = 1L;
     }.getType());
-    if (userDTO == null || userDTO.getId() ==null) {
+    if (userDTO.getId() ==null) {
       throw new APIException("Invalid contactId");
     }
     return userDTO;
