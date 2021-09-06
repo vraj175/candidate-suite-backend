88afa1cbb51eebca55f4ad82ea17e26ad0126a90
Urmil Patel <urmil.patel@aspiresoftserv.com>
2021-09-06 19:02:40 +0530

    Feature #37615: Candidate suite : Swagger Configuration

--- src/main/java/com/aspire/kgp/util/CompanyUtil.java
+++ src/main/java/com/aspire/kgp/util/CompanyUtil.java
@@ -1,10 +1,7 @@
 package com.aspire.kgp.util;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
