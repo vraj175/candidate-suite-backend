f1d1d944946c6ec0bd2a4d72a9f8d316b771fc04
Vivek Tanna <vivek.tanna@aspiresoftware.in>
2021-09-06 17:33:57 +0530

    Feature #37692: Candidate Suite : Write application test case.

--- src/main/java/com/aspire/kgp/service/impl/UserServiceImpl.java
+++ src/main/java/com/aspire/kgp/service/impl/UserServiceImpl.java
@@ -254,7 +254,7 @@ public class UserServiceImpl implements UserService {
   @Override
   public User saveOrUpdatePartner(String userName, String password) {
     User user = findByEmail(userName);
-    if (user == null || user.getRole().getName().equalsIgnoreCase(Constant.PARTNER)) {
+    if (user == null || Constant.PARTNER.equalsIgnoreCase(user.getRole().getName())) {
       String auth = userName + ":" + password;
       try {
         byte[] encodedAuth = Base64.getEncoder().encode(auth.getBytes(StandardCharsets.UTF_8));
