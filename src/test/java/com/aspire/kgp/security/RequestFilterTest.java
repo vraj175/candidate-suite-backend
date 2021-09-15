a9358943dff8bbbe5fc86eb624fee500192a4033
Vivek Tanna <vivek.tanna@aspiresoftware.in>
2021-09-15 16:18:29 +0530

    Feature #37692: Candidate Suite : Write application test case.

--- src/test/java/com/aspire/kgp/security/RequestFilterTest.java
+++ src/test/java/com/aspire/kgp/security/RequestFilterTest.java
@@ -1,17 +1,11 @@
 package com.aspire.kgp.security;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -23,10 +17,10 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import com.aspire.kgp.CustomTestData;
 import com.aspire.kgp.constant.Constant;
-import com.aspire.kgp.exception.MissingAuthTokenException;
 import com.aspire.kgp.model.User;
 import com.aspire.kgp.service.UserService;
 
@@ -41,9 +35,6 @@ class RequestFilterTest {
   @Mock
   UserService service;
 
-  @Mock
-  MockHttpServletRequest request;
-
   @BeforeEach
   void setUp() throws Exception {
     MockitoAnnotations.openMocks(this);
@@ -51,40 +42,52 @@ class RequestFilterTest {
 
   @Test
   void testDoFilterInternal() throws ServletException, IOException {
+    MockHttpServletRequest request = CustomTestData.getRequest();
     MockHttpServletResponse response = new MockHttpServletResponse();
-    when(request.getRequestURL()).thenReturn(new StringBuffer("/api/v1.0/oauth/token"));
+    request.setParameter("username", Constant.TEST);
+    request.setParameter("password", Constant.TEST);
     User user = CustomTestData.getUser();
     when(service.saveOrUpdatePartner(anyString(), anyString())).thenReturn(user);
+    request.setRequestURI("/api/v1.0/oauth/token");
     filter.doFilterInternal(request, response, filterChain);
-    verify(service, times(1)).saveOrUpdatePartner(null, null);
+    verify(service, times(1)).saveOrUpdatePartner(Constant.TEST, Constant.TEST);
 
-    when(request.getRequestURL()).thenReturn(new StringBuffer("/bpi/"));
+    request.setRequestURI("/bpi/");
     filter.doFilterInternal(request, response, filterChain);
-    verify(service, times(1)).saveOrUpdatePartner(null, null);
-    
-    when(request.getRequestURL()).thenReturn(new StringBuffer("/api/initialize"));
+    verify(service, times(1)).saveOrUpdatePartner(Constant.TEST, Constant.TEST);
+
+    request.setRequestURI("/api/initialize");
     filter.doFilterInternal(request, response, filterChain);
-    verify(service, times(1)).saveOrUpdatePartner(null, null);
-    
-    when(request.getRequestURL()).thenReturn(new StringBuffer("/api/user/invite"));
+    verify(service, times(1)).saveOrUpdatePartner(Constant.TEST, Constant.TEST);
+
+    request.setRequestURI("/api/user/invite");
     filter.doFilterInternal(request, response, filterChain);
-    verify(service, times(1)).saveOrUpdatePartner(null, null);
-    
-    when(request.getRequestURL()).thenReturn(new StringBuffer("/api/user/forgotPassword"));
+    verify(service, times(1)).saveOrUpdatePartner(Constant.TEST, Constant.TEST);
+
+    request.setRequestURI("/api/user/forgotPassword");
     filter.doFilterInternal(request, response, filterChain);
-    verify(service, times(1)).saveOrUpdatePartner(null, null);
-    
-    when(request.getRequestURL()).thenReturn(new StringBuffer("/api/user/resetPassword"));
+    verify(service, times(1)).saveOrUpdatePartner(Constant.TEST, Constant.TEST);
+
+    request.setRequestURI("/api/user/resetPassword");
     filter.doFilterInternal(request, response, filterChain);
-    verify(service, times(1)).saveOrUpdatePartner(null, null);
-    
-    when(request.getRequestURL()).thenReturn(new StringBuffer("/api/user/verify/recaptcha"));
+    verify(service, times(1)).saveOrUpdatePartner(Constant.TEST, Constant.TEST);
+
+    request.setRequestURI("/api/user/verify/recaptcha");
     filter.doFilterInternal(request, response, filterChain);
-    verify(service, times(1)).saveOrUpdatePartner(null, null);
-    
-    when(request.getRequestURL()).thenReturn(new StringBuffer("/api/user/profile"));
+    verify(service, times(1)).saveOrUpdatePartner(Constant.TEST, Constant.TEST);
+
+    ReflectionTestUtils.setField(filter, "apiSecretKey", "kgpc@nd!d@t3su!t3u@t");
+    request.addHeader(Constant.API_KEY,
+        "$2a$10$f3GPt8/.hwBGRNOUzxuHLe5/MfSIrK5L/tubjn2tSq33gxtPklZw");
+    request.setRequestURI("/api/user/profile");
     filter.doFilterInternal(request, response, filterChain);
-    verify(service, times(1)).saveOrUpdatePartner(null, null);
+    verify(service, times(1)).saveOrUpdatePartner(Constant.TEST, Constant.TEST);
+
+    request.addHeader(Constant.API_KEY,
+        "$2a$10$f3GPt8/.hwBGRNOUzxuHLe5/MfSIrK5L/tubjn2tSq33gxtPklZu");
+    request.addHeader(Constant.AUTHORIZATION, "Bearer " + Constant.TEST);
+    filter.doFilterInternal(request, response, filterChain);
+    verify(service, times(1)).saveOrUpdatePartner(Constant.TEST, Constant.TEST);
   }
 
 }
