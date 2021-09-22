1a66f3553424418c67c8073316b6cd0ce98226bc
Vivek Tanna <vivek.tanna@aspiresoftware.in>
2021-09-22 20:00:56 +0530

    Feature #37692: Candidate Suite : Write application test case.

--- src/test/java/com/aspire/kgp/util/ContactUtilTest.java
+++ src/test/java/com/aspire/kgp/util/ContactUtilTest.java
@@ -67,8 +67,8 @@ class ContactUtilTest {
     assertEquals(contactDTO.getToken(), response.getToken());
     assertEquals(contactDTO.getWorkEmail(), response.getWorkEmail());
     assertEquals(contactDTO.getWorkPhone(), response.getWorkPhone());
-    assertEquals(contactDTO.getCompany(), response.getCompany());
-    assertEquals(contactDTO.getJobHistory(), response.getJobHistory());
+    assertEquals(contactDTO.getJobHistory().size(), response.getJobHistory().size());
+    assertEquals(contactDTO.getBoardDetails().size(), response.getBoardDetails().size());
 
   }
 
