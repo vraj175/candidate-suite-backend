0e72779e3efd58b2930a7be6f73a31e185cb4f75
Poorav Solanki <poorav.solanki@aspiresoftserv.com>
2021-12-13 12:10:38 +0530

    COmmit Issue wiht invite candiate suite

--- src/main/java/com/aspire/kgp/service/impl/UserServiceImpl.java
+++ src/main/java/com/aspire/kgp/service/impl/UserServiceImpl.java
@@ -156,18 +156,14 @@ public class UserServiceImpl implements UserService {
       userDTO.setEmail(email);
 
       log.info("staring email sending...");
-      if (user.isPasswordReset()) {
         // mail for add user or mail for invite
-        log.info("mail for add user or mail for invite");
-        Map<String, String> staticContentsMap = StaticContentsMultiLanguageUtil
-            .getStaticContentsMap(language, Constant.EMAILS_CONTENT_MAP);
-        String mailSubject = staticContentsMap.get("candidate.suite.invitation.email.subject");
-        mailService.sendEmail(email, bcc, mailSubject, mailService.getEmailContent(request, userDTO,
-            staticContentsMap, Constant.CANDIDATE_INVITE_EMAIL_TEMPLATE, candidateDTO), null);
-      } else {
-        log.info("mail for add search");
-        // mail for add search
-      }
+    log.info("mail for add user or mail for invite");
+    Map<String, String> staticContentsMap = StaticContentsMultiLanguageUtil
+        .getStaticContentsMap(language, Constant.EMAILS_CONTENT_MAP);
+    String mailSubject = staticContentsMap.get("candidate.suite.invitation.email.subject");
+    mailService.sendEmail(email, bcc, mailSubject, mailService.getEmailContent(request, userDTO,
+        staticContentsMap, Constant.CANDIDATE_INVITE_EMAIL_TEMPLATE, candidateDTO), null);
+      
       response = true;
     } catch (Exception e) {
       log.info(e);
