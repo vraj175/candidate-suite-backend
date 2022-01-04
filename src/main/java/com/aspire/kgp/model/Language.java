82eb457c9432630a2c5991058ab65bec5e561485
vraj.patel <vraj.patel@aspiresoftserv.com>
2022-01-04 11:01:35 +0530

    Feature #38969: Candidate Push Notification

--- src/main/java/com/aspire/kgp/model/Language.java
+++ src/main/java/com/aspire/kgp/model/Language.java
@@ -2,6 +2,9 @@ package com.aspire.kgp.model;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Entity
 public class Language extends SuperSub {
 
